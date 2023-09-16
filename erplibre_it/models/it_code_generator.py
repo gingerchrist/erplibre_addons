3b7e33044257a12626f148edad677582d373c83a
Mathieu Benoit <mathben@technolibre.ca>
2023-09-16 02:08:41 -0400

    [UPD] erplibre_it: can execute code generator in docker

--- erplibre_it/models/it_code_generator.py
+++ erplibre_it/models/it_code_generator.py
@@ -16,3 +16,15 @@ class ItCodeGenerator(models.Model):
         comodel_name="it.code_generator.module",
         inverse_name="code_generator",
     )
+
+    def action_generate_code(self):
+        # Start with local storage
+        for rec in self:
+            if rec.it_workspace_id:
+                module_name = "magasin"
+                # cmd = f'./script/code_generator/new_project.py --keep_bd_alive -m gn -d ./addons/TechnoLibre_odoo-code-generator-template --config "{\"model\":[{\"name\":\"membre\",\"fields\":[{\"name\":\"courriel\",\"type\":\"char\"},{\"name\":\"adresse\",\"type\":\"char\"}]},{\"name\":\"personnage\",\"fields\":[{\"name\":\"membre\",\"type\":\"many2one\",\"relation\":\"membre\"},{\"name\":\"point_vie\",\"type\":\"integer\"},{\"name\":\"habilete\",\"type\":\"many2many\",\"relation\":\"habilete\"}]},{\"name\":\"habilete\",\"fields\":[{\"name\":\"nb_xp\",\"type\":\"integer\"}]}]}"'
+                cmd = (
+                    "cd /ERPLibre;./script/code_generator/new_project.py --keep_bd_alive"
+                    f" -m {module_name} -d addons/addons"
+                )
+                rec.it_workspace_id.exec_docker(cmd)
