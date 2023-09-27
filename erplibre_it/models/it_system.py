7f8e25a8c37ab08e49d5ddcdb1093647e84beec5
Mathieu Benoit <mathben@technolibre.ca>
2023-09-27 08:27:36 +0000

    [UPD] erplibre_it: comment to use async update output process local

--- erplibre_it/models/it_system.py
+++ erplibre_it/models/it_system.py
@@ -119,6 +119,23 @@ class ItSystem(models.Model):
         p = subprocess.Popen(
             cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE
         )
+        # TODO check https://www.cyberciti.biz/faq/python-run-external-command-and-get-output/
+        # TODO support async update output
+        # import subprocess, sys
+        # ## command to run - tcp only ##
+        # cmd = "/usr/sbin/netstat -p tcp -f inet"
+        #
+        # ## run it ##
+        # p = subprocess.Popen(cmd, shell=True, stderr=subprocess.PIPE)
+        #
+        # ## But do not wait till netstat finish, start displaying output immediately ##
+        # while True:
+        #     out = p.stderr.read(1)
+        #     if out == '' and p.poll() != None:
+        #         break
+        #     if out != '':
+        #         sys.stdout.write(out)
+        #         sys.stdout.flush()
         (output, err) = p.communicate()
         p_status = p.wait()
         result = output.decode()
